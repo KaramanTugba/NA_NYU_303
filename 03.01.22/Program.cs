c76e9cd4e2088b42206ad203640eb5b315fb1d11
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-01-03 10:29:45 +0300

    Add project files.

---
+++ 03.01.22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03._01._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
